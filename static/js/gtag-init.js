8d3d06583f6f014f644763b7e8e66154e6eab8d8
Eberson Ramos <ebersonramos11@gmail.com>
2025-08-14 22:59:02 -0300

    feat: integrate Google Tag Manager and update institutional pages

--- static/js/gtag-init.js
+++ static/js/gtag-init.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'AW-17413746282');
